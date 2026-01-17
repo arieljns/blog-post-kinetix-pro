288c6d96fc77e58de52a2aaaf4cf763c699d040f
arieljns1 <arieljns1@gmail.com>
2026-01-17 17:21:02 +0700

    feat(core): adding routing base for the upcoming api endpoint

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const req = require("express/lib/request");
+const routes = require("./routes/index");
 
 const express = require("express");
 
 
 const app = express()
+app.use("/", routes)
 
 
 app.listen(process.env.PORT || 3000, () => {
